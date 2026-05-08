b8a78dcba9252c63d0e1d34cb5a36b053d8931e1
Rey <bacolod2186@gmail.com>
2026-05-08 16:19:25 +0800

    connect login to deployed backend

--- src/features/login/services/loginService.ts
+++ src/features/login/services/loginService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/login";
+const API = `${process.env.REACT_APP_API_URL}/api/login`;
 
 export const loginUser = async (data: {
   email: string;
